9d2bb94d53554292b24abccc326e1d315e237e1d
khotovec <tom@Design-6.local>
2017-02-08 15:49:33 -0500

    imageframe testing

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+/*jslint devel: true */
+
+$(document).ready(function main() {
+    "use strict";
+    var specialLink = document.getElementById('customLink');
+    specialLink.style.display = 'none';
+});
